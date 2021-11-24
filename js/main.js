102440a4de5384770c3383cafb8d94bbef66084b
Nikith Kalu <nikithkalu@gmail.com>
2021-11-24 13:47:41 +1100

    Squashed commit of the following:
    commit f9cb820ded012a6eac74878a575b7d5f04445ef4
    Merge: 91ea347 7ebb421
    Author: Nikith Kalu <nikithkalu@gmail.com>
    Date:   Wed Nov 24 13:44:42 2021 +1100
    
        Merge branch 'preloader-fix' into typewriter-effect-testing
    
    commit 91ea3475bffeb250ba2e11ee1fb998f09878238f
    Author: Nikith Kalu <nikithkalu@gmail.com>
    Date:   Wed Nov 24 13:42:11 2021 +1100
    
        added typewriter cursor blinker
    
    commit 7ebb421f77db43d54684b1915ccec49dc976ea6d
    Author: Nikith Kalu <nikithkalu@gmail.com>
    Date:   Wed Nov 24 13:37:17 2021 +1100
    
        containers removed
    
    commit 772547cacc2f4d3250992978a0bc74b7b798417e
    Author: Nikith Kalu <nikithkalu@gmail.com>
    Date:   Wed Nov 24 13:31:29 2021 +1100
    
        preloader removal
    
    commit 5e21418272885128f13401841613c99a511e106f
    Author: Nikith Kalu <nikithkalu@gmail.com>
    Date:   Wed Nov 24 13:19:23 2021 +1100
    
        Typewriter feature revision v2
    
    commit 9a42cf97a71ed74a3af7b2149a3e47c47ca3b6bc
    Author: Nikith Kalu <nikithkalu@gmail.com>
    Date:   Wed Nov 24 11:47:07 2021 +1100
    
        first test
    
        typewriter effect on heading

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-/* ===================================================================
- * Ethos 1.0.0 - Main JS
- *
- * ------------------------------------------------------------------- */
 
 (function($) {
 
@@ -23,29 +19,6 @@
 
    /* preloader
     * -------------------------------------------------- */
-    const ssPreloader = function() {
-
-        $("html").addClass('ss-preload');
-
-        $WIN.on('load', function() {
-
-            // force page scroll position to top at page refresh
-            $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            }); 
-            
-            // for hero content animations 
-            $("html").removeClass('ss-preload');
-            $("html").addClass('ss-loaded');
-
-        });
-    };
-
-
 
    /* pretty print
     * -------------------------------------------------- */
@@ -56,7 +29,75 @@
         });
     };
 
-
+   /* typewriter heading
+    * -------------------------------------------------- */
+   // ES6 Class
+class TypeWriter {
+    constructor(txtElement, words, wait = 3000) {
+      this.txtElement = txtElement;
+      this.words = words;
+      this.txt = '';
+      this.wordIndex = 0;
+      this.wait = parseInt(wait, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+  
+    type() {
+      // Current index of word
+      const current = this.wordIndex % this.words.length;
+      // Get full text of current word
+      const fullTxt = this.words[current];
+  
+      // Check if deleting
+      if(this.isDeleting) {
+        // Remove char
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        // Add char
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+  
+      // Insert txt into element
+      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  
+      // Initial Type Speed
+      let typeSpeed = 300;
+  
+      if(this.isDeleting) {
+        typeSpeed /= 2;
+      }
+  
+      // If word is complete
+      if(!this.isDeleting && this.txt === fullTxt) {
+        // Make pause at end
+        typeSpeed = this.wait;
+        // Set delete to true
+        this.isDeleting = true;
+      } else if(this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        // Move to next word
+        this.wordIndex++;
+        // Pause before start typing
+        typeSpeed = 500;
+      }
+  
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
+  
+  
+  // Init On DOM Load
+  document.addEventListener('DOMContentLoaded', init);
+  
+  // Init App
+  function init() {
+    const txtElement = document.querySelector('.txt-type');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+  }
 
    /* move header
     * -------------------------------------------------- */
@@ -317,8 +358,7 @@
    /* initialize
     * ------------------------------------------------------ */
     (function ssInit() {
-
-        ssPreloader();
+        
         ssPrettyPrint();
         ssMoveHeader();
         ssMobileMenu();
