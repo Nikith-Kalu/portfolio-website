e2f251a8f7968811695dc8019cf2fbdbdbb297ba
Nikith Kalu <nikithkalu@gmail.com>
2021-11-29 00:42:14 +1100

    visit counter
    addition of the visit counter

--- js/main.js
+++ js/main.js
@@ -98,7 +98,17 @@ class TypeWriter {
     // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
   }
-
+    /* visit counter
+    * -------------------------------------------------- */
+   const countEl = document.getElementById('count');
+   
+   function updateVisitCount() {
+       fetch('https://api.countapi.xyz/update/nikith-kalu/nkvisit/?amount=1')
+            .then(res => res.json())
+            .then(res => {
+                countEl.innerHTML = res.value;
+            });
+   }
    /* move header
     * -------------------------------------------------- */
     const ssMoveHeader = function () {
